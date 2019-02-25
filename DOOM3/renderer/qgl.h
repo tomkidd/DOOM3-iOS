2f1b006293f8de63680ccba33e443bbdd9d0bea9
Tom Kidd <tomkidd@gmail.com>
2019-02-25 17:15:59 -0600

    Tweaks to aid compilation

--- DOOM3/renderer/qgl.h
+++ DOOM3/renderer/qgl.h
@@ -42,7 +42,11 @@ If you have questions concerning this license or the applicable additional terms
 	#endif
 #endif
 
+#ifdef IOS
+#include <SDL_opengles.h>
+#else
 #include <SDL_opengl.h>
+#endif
 
 #if defined( ID_DEDICATED ) && defined( _WIN32 )
 // restore WINGDIAPI
